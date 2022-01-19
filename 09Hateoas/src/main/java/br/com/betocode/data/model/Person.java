1a3e113da2fbefd1ef519c951d689de87e83cd3b
PauloCarpinetti <pauloscarpinetti@gmail.com>
2022-01-19 03:00:14 -0300

    Inclusão de Haeteoas

---
+++ 09Hateoas/src/main/java/br/com/betocode/data/model/Person.java
@@ -0,0 +1,100 @@
+package br.com.betocode.data.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pessoa")
+public class Person implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="primeiro_nome", nullable = false, length= 100)
+	private String primeiroNome;
+	
+	@Column(name="ultimo_nome", nullable = false, length= 100)
+	private String ultimoNome;
+	
+	@Column(name="endereco", length= 100)
+	private String endereco;
+	
+	@Column(name="genero", length= 9)
+	private String genero;
+	
+	public Person() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(endereco, genero, id, primeiroNome, ultimoNome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(endereco, other.endereco) && Objects.equals(genero, other.genero)
+				&& Objects.equals(id, other.id) && Objects.equals(primeiroNome, other.primeiroNome)
+				&& Objects.equals(ultimoNome, other.ultimoNome);
+	}
+	
+	
+
+}
